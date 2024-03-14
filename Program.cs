a5211dd5b68b488284a1724270cd626322536a38
Tamara Terekhova <terekhova.toma@inbox.ru>
2024-03-14 15:49:59 +0700

    редакция задачи

--- Program.cs
+++ Program.cs
@@ -1,61 +1,71 @@
-﻿﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
-// длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
-// либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
-// лучше обойтись исключительно массивами.
+﻿Commands();
+string[] array = new string[] {};
 
-// Примеры:
-// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
-// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
-// [“Russia”, “Denmark”, “Kazan”] → []
-
-string[] FillArray()
+string fromUser = ReadInput("Введите команду: ");
+switch (fromUser)
 {
-    Console.Write("Введите размер массива: ");
-    int size = Convert.ToInt32(Console.ReadLine());
-    string[] array = new string[size];
-    for (int i = 0; i < size; i += 1)
-    {
-        Console.Write($"Введите {i + 1} элемент массива: ");
-        array[i] = Console.ReadLine();
-    }
-    return array;
+    case "1":
+        array = new string[] { "Hello", "2", "world", ":-)" };
+        break;
+    case "2":
+        array = new string[] { "1234", "1567", "-2", "computer science" };
+        break;
+    case "3":
+        array = new string[] { "Russia", "Denmark", "Kazan" };
+        break;
+    default:
+        Console.WriteLine($"{fromUser} - Такой команды нет");
+        break;
 }
 
-void PrintArray(string[] array)
+
+int lenNewArray = 0;
+for (int i = 0; i <= array.Length - 1; i++)
 {
-    Console.Write("[");
-    for (int i = 0; i < array.Length - 1; i += 1)
-        Console.Write($"“{array[i]}”, ");
-    Console.Write($"“{array[array.Length-1]}”]");
+    if (array[i].Length <= 3) lenNewArray++;
+}
+
+string[] newArray = new string[lenNewArray];
+int idx = 0;
+
+for (int i = 0; i <= array.Length - 1; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        newArray[idx] = array[i];
+        idx++;
+    }
+}
+
+PrintArray(array);
+Console.Write("→ ");
+PrintArray(newArray);
+
+// Функция: Вывод команд для работы с программой
+void Commands()
+{
+    Console.WriteLine();
+    Console.WriteLine("СПИСОК КОМАНД:");
+    Console.WriteLine("1 – использовать массив: [“Hello”, “2”, “world”, “:-)”]");
+    Console.WriteLine("2 – использовать массив: [“1234”, “1567”, “-2”, “computer science”]");
+    Console.WriteLine("3 – использовать массив: [“Russia”, “Denmark”, “Kazan”]");
     Console.WriteLine();
 }
 
-
-string[] ResultArray(string[] array)
+// Функция ввода
+string ReadInput(string msg)
 {
-    int sz = 0;
-    int j = 0;
-    for (int i = 0; i < array.Length; i += 1)
-    {
-        if (array[i].Length <= 3) sz += 1;
-    }
-    string[] arrayRes = new string[sz];
-    for (int i = 0; i < array.Length; i += 1)
-     {
-        if (array[i].Length <= 3)
-        {
-            arrayRes[j] = array[i];
-            j += 1;
-        }
-    }
-    return arrayRes;
+    Console.Write(msg);
+    return Console.ReadLine();
 }
 
-string[] arraySet = FillArray();
-Console.WriteLine();
-Console.WriteLine("Первоначально заданный массив:");
-PrintArray(arraySet);
-Console.WriteLine();               
-string[] arrayRes = ResultArray(arraySet);
-Console.WriteLine("Новый массив из строк, длина которых меньше, либо равна 3 символам:");
-PrintArray(arrayRes);
+//  Функция вывода массива в терминал
+void PrintArray(string[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"“{array[i]}”, ");
+    }
+    Console.Write("] ");
+}
